f929308219613e3bf462fbd6c0d6a802824fea89
Simon Jones <github@simonjjones.com>
2020-12-17 00:15:16 -0500

    import lavish.nix and remove rustup. Rust dev will only happen in environs

--- nixfiles/configuration.nix
+++ nixfiles/configuration.nix
@@ -4,6 +4,7 @@
     [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
       ./coregui.nix
       ./corecli.nix
+      ./lavish.nix
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
--- nixfiles/corecli.nix
+++ nixfiles/corecli.nix
@@ -54,7 +54,6 @@ in {
       pkgs.pinentry
       pkgs.ripgrep
       pkgs.rsync
-      pkgs.rustup
       pkgs.tig
       pkgs.tldr
       pkgs.tmux
