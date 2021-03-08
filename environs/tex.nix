de8cfe47b134e7addb290d2069dc983401a7c824
Simon Jones <github@simonjjones.com>
2021-03-07 19:56:20 -0500

    latex notes

--- environs/tex.nix
+++ environs/tex.nix
@@ -13,8 +13,9 @@ in
     shellHook = ''
       export NIX_SHELL_NAME='${shellname}'
       alias p='pandoc -o out.pdf --pdf-engine tectonic'
-    '';
+   '';
   }
+
 
 # This yaml header helps
 # ---
@@ -24,4 +25,12 @@ in
 # output: pdf_document
 # fontsize: 12pt
 # ---
+# ---
+# title: "Habits"
+# author: John Doe
+# date: March 22, 2005
+# geometry: "left=3cm,right=3cm,top=2cm,bottom=2cm"
+# output: pdf_document
+# ---
 
+# https://oeis.org/wiki/List_of_LaTeX_mathematical_symbols
